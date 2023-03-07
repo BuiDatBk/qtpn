abe49a56f90f352b48459b9e8d6209e2097476b8
BuiDatBk <118350748+BuiDatBk@users.noreply.github.com>
2023-03-08 00:18:23 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Gửi các chị em!',
+    introDesc: ` Ngày 8/3 chúc một nửa thế giới luôn thành công trong cuộc sống! Chúc bạn luôn duyên dáng và xinh đẹp trong mắt một nửa thế giới còn lại!`,
+    btnIntro: 'hihi',
+    title: 'Lời chúc này chắc hẳn làm cho ngày 8/3 của bạn vui nhiều nhiều 😙',
+    desc: 'Vậy bạn có thấy tôi đẹp zai khum !',
+    btnYes: 'Vẫn cứ là oce <333',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Bạn hãy gửi lời gì đó với tôi đi <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Anh dễ thương lại đẹp trai nữa <33333333',
+    mess: 'Anh biết mà 🥰',
+    messDesc: 'Còn chờ gì nữa mà không inbox cho a đi nào kk.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/dat.rongvang.73' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
